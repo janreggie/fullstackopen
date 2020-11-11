8bf3c3fb329bc61cc815d54bd9fb8a08cf4ec0a3
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2020-11-11 16:15:14 +0800

    exercise 1.5: dismantle other components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,33 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map(elem => <Part name={elem.name} exercises={elem.exercises} />)}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  // well... reduce works
-  return (
-    <p>Number of exercises {props.parts.reduce((accum, current) => accum + current.exercises, 0)}</p>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
@@ -47,9 +20,12 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>{course}</h1>
+      {parts.map(elem =>
+        <p>{elem.name} {elem.exercises}</p>)}
+      <p>
+        Number of exercises {parts.reduce((accum, current) => accum + current.exercises, 0)}
+      </p>
     </div>
   )
 }
