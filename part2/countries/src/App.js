68e8131f01185fb93cabc354ed0e779382eeed61
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-05-13 16:12:03 +0800

    part2/countries: updated logic, updates, rearranged
    * updates to packages to fix vulns
    * changed logic in getting weather data
    * rearranged components to make it look better by putting App and more
      relevant shit to the top while helper fxns go at bottom

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,74 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-/** BasicInfo displays basic info of a country  */
-const BasicInfo = ({ country }) => (
-  <div>
-    <div>capital {country.capital}</div>
-    <div>population {country.population}</div>
-  </div>
-)
-
-/** Languages displays the languages of a country */
-const Languages = ({ languages }) => (
-  <div>
-    <h2>languages</h2>
-    <ul>
-      {languages.map(lang => <li key={lang.iso639_1}>{lang.name} ({lang.nativeName})</li>)}
-    </ul>
-  </div>
-)
-
-/** Flag displays a country's flag */
-const Flag = ({ country }) => (
-  <div>
-    <img src={country.flag} alt={`Flag of ${country.name}`} width='200px' />
-  </div>
-)
-
-/** Weather displays a city's weather */
-const Weather = ({ city }) => {
-  const [weatherData, setWeatherData] = useState({})
-  const API_KEY = process.env.REACT_APP_API_KEY // for weatherstack
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    const params = new URLSearchParams({
-      'q': city,
-      'appid': API_KEY,
-      'units': 'metric'
-    })
-    axios.get('https://api.openweathermap.org/data/2.5/weather?' + params.toString())
+    axios.get('https://restcountries.eu/rest/v2/all')
       .then(
-        response => setWeatherData(response.data),
-        reason => console.log(`Couldn't get weather data from ${city}`, reason)
+        response => setCountries(response.data),
+        reason => console.log("Couldn't get from https://restcountries.eu/rest/v2/all,", reason)
       )
-  }, [city, API_KEY])
+  }, [])
 
-  console.log('weatherData before err check', weatherData)
-
-  if (!weatherData.weather) {
-    return (
-      <div>
-        Fetching data for {city}...
-      </div>
-    )
-  }
-
-  console.log('weatherData after err check', weatherData)
+  /** nameContainsFilter takes in country data and returns if it contains filter (case-insensitive) */
+  const nameContainsFilter = (country) => country.name.toLowerCase().includes(filter.toLowerCase())
 
   return (
     <div>
-      <h2>Weather in {city}</h2>
-      <div>
-        <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].description} />
-        {weatherData.weather[0].description}
-      </div>
-      <div>
-        <strong>temperature:</strong> {weatherData.main.temp} °C
-        (feels like {weatherData.main.feels_like} °C)</div>
-      <div>
-        <strong>wind:</strong> {weatherData.wind.speed} km/h direction {weatherData.wind.deg}°
-      </div>
+      <Filterer filter={filter} setFilter={setFilter} />
+      <Display countries={countries.filter(country => nameContainsFilter(country))} />
+    </div>
+  );
+}
+
+const Filterer = ({ filter, setFilter }) => {
+  return (
+    <div>
+      find countries
+      <input value={filter} onChange={e => setFilter(e.target.value)} />
+    </div>
+  )
+}
+
+/**
+ * Display displays a list of already filtered countries.
+ * 
+ * @param {{countries: any[]}}
+ */
+const Display = ({ countries }) => {
+  if (countries.length > 10) {
+    return (<div>Too many matches, specify another filter</div>)
+  }
+
+  if (countries.length === 1) {
+    return <DisplaySingle country={countries[0]} />
+  }
+
+  return (
+    <div>
+      {countries.map(country => <DisplayItem key={country.alpha2Code} country={country} />)}
     </div>
   )
 }
@@ -103,57 +84,69 @@ const DisplayItem = ({ country }) => {
   )
 }
 
-/**
- * Display displays a list of already filtered countries.
- * 
- * @param {{countries: any[]}}
- */
-const Display = ({ countries }) => {
-  if (countries.length > 10) {
-    return (<div>Too many matches, specify another filter</div>)
-  }
+/** BasicInfo displays basic info of a country  */
+const BasicInfo = ({ country }) => (
+  <div>
+    <div>capital {country.capital}</div>
+    <div>population {country.population}</div>
+  </div>
+)
 
-  if (countries.length === 1) {
-    return <DisplaySingle country={countries[0]} />
-  }
+/** Languages displays the languages of a country */
+const Languages = ({ languages }) => (
+  <div>
+    <h2>languages</h2>
+    <ul>
+      {languages.map(lang => <li key={lang.iso639_1}>{lang.name} ({lang.nativeName})</li>)}
+    </ul>
+  </div>
+)
 
-  return (
+/** Flag displays a country's flag */
+const Flag = ({ country }) => (
+  <div>
+    <img src={country.flag} alt={`Flag of ${country.name}`} width='200px' />
+  </div>
+)
+
+/** Weather displays a city's weather */
+const Weather = ({ city }) => {
+  const [output, setOutput] = useState(<div>Fetching data for {city}...</div>)
+  const API_KEY = process.env.REACT_APP_API_KEY // for weatherstack
+
+  const parseWeatherData = (weatherData) => (
     <div>
-      {countries.map(country => <DisplayItem key={country.alpha2Code} country={country} />)}
+      <h2>Weather in {city}</h2>
+      <div>
+        <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].description} />
+        {weatherData.weather[0].description}
+      </div>
+      <div>
+        <strong>temperature:</strong> {weatherData.main.temp} °C
+        (feels like {weatherData.main.feels_like} °C)</div>
+      <div>
+        <strong>wind:</strong> {weatherData.wind.speed} km/h direction {weatherData.wind.deg}°
+      </div>
     </div>
   )
-}
-
-const Filterer = ({ filter, setFilter }) => {
-  return (
-    <div>
-      find countries
-      <input value={filter} onChange={e => setFilter(e.target.value)} />
-    </div>
-  )
-}
-
-const App = () => {
-  const [countries, setCountries] = useState([])
-  const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios.get('https://restcountries.eu/rest/v2/all')
+    const params = new URLSearchParams({
+      'q': city,
+      'appid': API_KEY,
+      'units': 'metric'
+    })
+    axios.get('https://api.openweathermap.org/data/2.5/weather?' + params.toString())
       .then(
-        response => setCountries(response.data),
-        reason => console.log("Couldn't get from https://restcountries.eu/rest/v2/all,", reason)
+        response => setOutput(parseWeatherData(response.data)),
+        reason => {
+          console.log(`Couldn't get weather data from ${city}`, reason)
+          setOutput(<div>Could not get weather data for {city}</div>)
+        }
       )
-  }, [])
+  }, [city, API_KEY])
 
-  /** nameContainsFilter takes in country data and returns if it contains filter (case-insensitive) */
-  const nameContainsFilter = (country) => country.name.toLowerCase().includes(filter.toLowerCase())
-
-  return (
-    <div>
-      <Filterer filter={filter} setFilter={setFilter} />
-      <Display countries={countries.filter(country => nameContainsFilter(country))} />
-    </div>
-  );
+  return output
 }
 
 export default App;
