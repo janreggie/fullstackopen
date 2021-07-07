c0e30bc8d0e7a19c3985346832bf3c7f2d1927e6
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-07-07 15:44:42 +0800

    4.1, 4.2: Blog list step1, step2

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,23 +1,9 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import http from 'http'
 import config from './utils/config.js'
 import logger from './utils/logger.js'
-import blogRouter from './controllers/blogs.js'
+import app from './app.js'
 
-const app = express()
-
-mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
-
-app.listen(config.PORT, () => {
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -1,4 +1,15 @@
 import mongoose from 'mongoose'
+import logger from '../utils/logger.js'
+import config from '../utils/config.js'
+
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error('error connecting to MongoDB:', error.message))
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -6,6 +17,14 @@ const blogSchema = new mongoose.Schema({
   url: String,
   likes: Number
 })
+blogSchema.set('toJSON',
+  {
+    transform: (_document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 
 const Blog = mongoose.model('Blog', blogSchema)
 export default Blog
