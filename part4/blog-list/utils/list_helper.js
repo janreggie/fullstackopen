894f4ddc83c3572b0bbd7ed7ac267f85c5871039
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-07-08 16:13:16 +0800

    4.5: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -8,7 +8,27 @@ const dummy = (blogs) => {
 //
 const totalLikes = (blogs) =>
   blogs.map(blog => blog.likes)
-    .filter(v => v) // remove items without likes param, if any
+    .filter(v => v != null) // remove items without likes param, if any
     .reduce((c, s) => c + s, 0)
 
-export default { dummy, totalLikes }
+// favoriteBlog returns the blog wtih the most likes.
+// If there are many top favorites, it's enough to return one of them
+//
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) { return {} }
+
+  // See https://stackoverflow.com/a/6102340/14020202 for max of array
+  const maxLikes = Math.max.apply(Math,
+    blogs.map(blog => blog.likes)
+      .filter(v => v != null))
+  const found = blogs.filter(blog => blog.likes === maxLikes)[0]
+  return {
+    id: found._id || found.id,
+    title: found.title,
+    author: found.author,
+    url: found.url,
+    likes: found.likes
+  }
+}
+
+export default { dummy, totalLikes, favoriteBlog }
