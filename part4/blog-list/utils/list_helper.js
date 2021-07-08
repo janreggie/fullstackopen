185d70730507beb36d986c949ce1d4f8e8a2be00
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-07-08 15:44:58 +0800

    4.4: helper functions and unit tests, step2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,14 @@
+// dummy is a dummy funciton that returns 1 always
 const dummy = (blogs) => {
   return 1
 }
 
-export default { dummy }
+// totalLikes returns how many likes are there in total
+// from a list of blogs that it is passed on to.
+//
+const totalLikes = (blogs) =>
+  blogs.map(blog => blog.likes)
+    .filter(v => v) // remove items without likes param, if any
+    .reduce((c, s) => c + s, 0)
+
+export default { dummy, totalLikes }
